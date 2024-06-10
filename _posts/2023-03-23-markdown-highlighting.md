df75b6d0b549cd1e0076a18a6145499819c1a67d
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-06-10 14:45:25 +0900

    202406101445

--- _posts/2023-03-23-markdown-highlighting.md
+++ _posts/2023-03-23-markdown-highlighting.md
@@ -5,6 +5,42 @@ categories: test
 ---
 <!-- markdownlint-disable -->
 
+### Plain
+
+```plain
+{
+  "thisSyntax": error
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+```
+
+### CSS
+
+This is use triple backticks <code>```css</code>
+
+```css
+/* This is a single-line comment */
+
+h1 {
+    color: blue;
+    text-decoration: underline;
+}
+```
+
+{% raw %}This is use `{% highlight css linenos %}`{% endraw %}
+
+{% highlight css linenos %}
+/* This is a single-line comment */
+
+body {
+    font-family: Arial, sans-serif;
+    color: #333;
+}
+
+{% endhighlight %}
+
 ### Diff
 
 ```diff
@@ -34,38 +70,6 @@ function addTwoNumbers (num1, num2) {
         </script>
     </body>
 </html>
-```
-
-### CSS
-
-{% raw %}This is use `{% highlight css linenos %}`{% endraw %}
-
-{% highlight css linenos %}
-/* This is a single-line comment */
-
-body {
-    font-family: Arial, sans-serif;
-    color: #333;
-}
-
-h1 {
-    color: blue;
-    text-decoration: underline;
-}
-{% endhighlight %}
-
-```css
-/* This is a single-line comment */
-
-body {
-    font-family: Arial, sans-serif;
-    color: #333;
-}
-
-h1 {
-    color: blue;
-    text-decoration: underline;
-}
 ```
 
 ### SCSS
--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,101 @@
     });
   }
 
+  // Go to Top
+  document.addEventListener("DOMContentLoaded", function () {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        document.getElementById("scroll-top__link").classList.add("visible");
+      } else {
+        document.getElementById("scroll-top__link").classList.remove("visible");
+      }
+    }
+
+    document
+      .getElementById("scroll-top__link")
+      .addEventListener("click", function () {
+        document.body.scrollTop = 0; /* For Safari */
+        document.documentElement.scrollTop = 0; /* For other browsers */
+      });
+  });
+
+  // Navigation menu
+  document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLink = document.querySelectorAll(".nav-link");
+
+    hamburger.addEventListener("click", mobileMenu);
+    navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+    function mobileMenu() {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    }
+
+    function closeMenu() {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    }
+  });
+
+  // Copy to clipboard
+  document.addEventListener("DOMContentLoaded", function () {
+    const clipboardButtons = document.querySelectorAll(
+      '.code-header button[aria-label="copy"]'
+    );
+    clipboardButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        const codeBlock = button
+          .closest(".code-header")
+          .nextElementSibling.querySelector(".rouge-code");
+        const text = codeBlock.innerText.trim();
+
+        navigator.clipboard
+          .writeText(text)
+          .then(function () {
+            button.innerHTML = '<i class="ti ti-check"></i>';
+            showTooltip(button, "Copied!");
+            setTimeout(function () {
+              button.innerHTML = '<i class="ti ti-clipboard"></i>';
+            }, 2000);
+          })
+          .catch(function (err) {
+            console.error("Failed to copy!", err);
+          });
+      });
+    });
+
+    function showTooltip(element, message) {
+      const tooltip = document.createElement("div");
+      tooltip.className = "tooltip";
+      tooltip.textContent = message;
+      document.body.appendChild(tooltip);
+
+      const rect = element.getBoundingClientRect();
+      tooltip.style.left = `${
+        rect.left + window.scrollX + rect.width / 2 - tooltip.offsetWidth / 2
+      }px`;
+      tooltip.style.top = `${
+        rect.top + window.scrollY - tooltip.offsetHeight - 5
+      }px`;
+
+      tooltip.classList.add("show");
+
+      setTimeout(function () {
+        tooltip.classList.remove("show");
+        document.body.removeChild(tooltip);
+      }, 2000);
+    }
+  });
+
   // Theme switcher
   // Thanks to Aleksandr Hovhannisyan <https://www.aleksandrhovhannisyan.com/blog/the-perfect-theme-switch/>
   const Theme = { AUTO: "auto", LIGHT: "light", DARK: "dark" };
